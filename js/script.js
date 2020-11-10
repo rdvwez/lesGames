9d9af791e63f911474a7eaa3ab808744644a3037
vwez <desirv63@gmail.com>
2020-11-10 18:35:26 +0100

    premiere modification

--- js/script.js
+++ js/script.js
@@ -4,3 +4,34 @@
 particlesJS.load('particles-js', '../particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
+
+  /*=====================================
+  code js du bouton catch me
+  ======================================*/
+  const button = document.getElementById("runaway-btn");
+  console.log(button)
+
+  const animateMove = (element, prop, pixels) =>
+    anime({
+      targets: element,
+      [prop]: `${pixels}px`,
+      easing: "easeOutCirc"
+    });
+  
+  ["mouseover", "click"].forEach(function (el) {
+    button.addEventListener(el, function (event) {
+      const top = getRandomNumber(window.innerHeight - this.offsetHeight);
+      const left = getRandomNumber(window.innerWidth - this.offsetWidth);
+  
+      animateMove(this, "left", left).play();
+      animateMove(this, "top", top).play();
+    });
+  });
+  
+  const getRandomNumber = (num) => {
+    return Math.floor(Math.random() * (num + 1));
+  };
+  
+  /*=====================================
+  fin code js du bouton catch me
+  ======================================*/
